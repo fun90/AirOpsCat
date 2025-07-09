b1f91bff5e2799c84c81ede2fe36213ba1bf1861
fun90 <congzi@live.com>
2025-07-09 23:11:49 +0800

    cdn

--- src/main/resources/static/js/device/server.js
+++ src/main/resources/static/js/device/server.js
@@ -1,6 +1,6 @@
 
 import { DataTable } from '/static/js/common/data-table.js';
-import { Modal } from '/static/tabler/js/tabler.esm.min.js';
+import { Modal } from 'https://cdn.jsdelivr.net/npm/@tabler/core@1.3.2/dist/js/tabler.esm.min.js';
 
 const serverTable = new DataTable({
     data: {
--- src/main/resources/static/js/money/transactions.js
+++ src/main/resources/static/js/money/transactions.js
@@ -1,7 +1,7 @@
 
 import { DataTable } from '/static/js/common/data-table.js';
 import { createSearchDropdown, SearchDropdownPresets, ValidationRules } from '/static/js/common/search-dropdown.js';
-import ApexCharts from '/static/tabler/libs/apexcharts/dist/apexcharts.esm.js';
+import ApexCharts from 'https://cdn.jsdelivr.net/npm/apexcharts@4.7.0/+esm';
 
 const transactionTable = new DataTable({
     data: {
