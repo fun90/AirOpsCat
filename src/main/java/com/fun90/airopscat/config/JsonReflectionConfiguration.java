1c15322e7075bd8d3e4e157634c66163c09d4c36
fun90 <congzi@live.com>
2026-04-02 22:03:11 +0800

    fix native mysql startup

--- src/main/java/com/fun90/airopscat/config/JsonReflectionConfiguration.java
+++ src/main/java/com/fun90/airopscat/config/JsonReflectionConfiguration.java
@@ -121,6 +121,9 @@ import io.quarkus.runtime.annotations.RegisterForReflection;
     com.fun90.airopscat.model.entity.RouteRule.class,
     com.fun90.airopscat.model.entity.SystemConfig.class,
 
+    // MySQL JDBC classes loaded reflectively in native mode
+    com.mysql.cj.PerConnectionLRUFactory.class,
+
     // Time-related classes (Java standard classes are automatically registered)
     java.time.LocalDateTime.class,
     java.time.LocalDate.class,
